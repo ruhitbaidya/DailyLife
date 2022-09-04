01e03518073bf19dab6e88279aebe505573917a0
ruhitbaidya <ruhitbaidya01@gmail.com>
2022-09-05 00:43:47 +0600

    add time show condition

--- indexJsFile/oclock.js
+++ indexJsFile/oclock.js
@@ -5,13 +5,34 @@ function oclock(){
     let hours = data.getHours();
     let munits = data.getMinutes();
     let second = data.getSeconds();
+    let zoon = '';
     
     
-    document.getElementById('timeShow').innerText = `${hours} : ${munits} : ${second}`
 
-   
+    if(second <= 9){
+        second = '0' + second;
+    }
+    if(hours >= 12){
+        hours =  hours - 12;
+        zoon = 'PM'
+    }else if(hours == 0){
+        hours = '12';
+        zoon = 'AM';
+    }else{
+        hours = hours;
+        zoon = 'AM';
+    }
+    if(hours < 9){
+        hours = '0' + hours;
+    }
+    
+    if(munits < 9){
+        munits = '0' + munits;
+    }
 
 
+
+   document.getElementById('timeShow').innerText = `${hours} : ${munits} : ${second}  ${zoon}`
 
 
     setInterval(() => {
